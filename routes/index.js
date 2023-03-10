80351c1dee8bdec49f38f1df1a7ec9c893e92e95
Ben Williamson <jhmr33@durham.ac.uk>
2023-03-10 15:53:07 +0000

    slight change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MoveIn' });
+  res.render('index', { title: 'MoveIn (slight change)' });
 });
 
 module.exports = router;
