26007ae2a102d36873ae27066b143def7712d4fd
Ben Williamson <benmacwill@gmail.com>
2023-03-13 14:57:46 +0000

    more changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MoveIn (slight change)' });
+  res.render('index', { title: 'MoveIn (slight change (again))' });
 });
 
 module.exports = router;
