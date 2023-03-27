5b2a327e6b6762ff01248f1ddbba27beaea9c33a
Ben Williamson <jhmr33@durham.ac.uk>
2023-03-27 16:16:08 +0100

    Added JWT to maintain session

---
+++ routes/signUp.post.js
@@ -0,0 +1,31 @@
+var jwt = require('jsonwebtoken');
+
+module.exports = (app, User) => {
+    app.post("/signUp", async (req, res) => {
+        let response = {};
+
+        let user = await User.findOne({email: req.body.email});
+
+        if(user) {
+            res.status(400).send("Username taken.");
+            return
+        }
+
+        try {
+            let newUser = await User.create(req.body);
+
+            let token = jwt.sign({
+                email: newUser.email
+            }, process.env.SECRET, { expiresIn: '2h'});
+            
+            newUser.update({token})
+
+            response.message = "User created."
+        }
+        catch(error) {
+            let message = error.errors[0].message;
+            response.message = message ? message : "Unknown error occured.";
+        }
+        res.send(response);
+    })
+}
